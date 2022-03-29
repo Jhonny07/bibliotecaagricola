eb7311522e753d4691a6540fcf2717c5cf0ca701
Jhonny07 <jhonnyjacinto09@icloud.com>
2022-03-29 18:48:38 +0000

    Adicionando Arquivos

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -1,7 +1,12 @@
 package main.java.application;
+import org.springframework.boot.SpringBootApplication;
 
-
+@SpringBootApplication
 public class application {
+
+    public static void main (String[] args) {
+        SpringBootApplication.run(Application.class, args);
+    }
 
 }
     
